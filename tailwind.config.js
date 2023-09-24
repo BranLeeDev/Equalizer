8205359e6da10852e1cd50a2cacfa22a804c5925
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-24 10:50:21 -0500

    Pequeños cambios en el html, configuración adicional en input.css y tailwin.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,17 @@ module.exports = {
       colors: {
         main: "#191826",
         secondary: "#FCFAF9",
+        "buy-card": "#FA7453",
       },
       fontFamily: {
         "ibm-plex": ["IBM Plex Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        mobile: "url('/src/assets/images/png/bg-main-mobile.png')",
+        "pattern-one": "url('/src/assets/images/svg/bg-pattern-1.svg')",
+        "pattern-two": "url('/src/assets/images/svg/bg-pattern-2.svg')",
+        "icon-ios": "url('/src/assets/images/svg/icon-apple.svg')",
+        "icon-android": "url('/src/assets/images/svg/icon-android.svg')",
       },
     },
   },
